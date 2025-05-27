3e0b0d4750244c88476c9ec4b5cb02ca7222e638
Bienvenu2004 <kidsglory1@gmail.com>
2025-05-27 15:39:05 +0100

    feat: added hello endpoint

--- src/main/java/com/example/support_service/SupportServiceApplication.java
+++ src/main/java/com/example/support_service/SupportServiceApplication.java
@@ -2,12 +2,21 @@ package com.example.support_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SupportServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportServiceApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
